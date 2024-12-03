f7d8227eba7c001d47c2ab3a0c2a418bebbc86a3
AmrZeina <142619031+AmrZeina@users.noreply.github.com>
2024-12-03 17:40:34 +0200

    new branch

---
+++ Connect-Hub/src/main/java/Backend/ConnectHub.java
@@ -0,0 +1,10 @@
+
+package Backend;
+
+
+public class ConnectHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Connect-Hub/src/main/java/Backend/Content.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+/**
+ *
+ * @author amrze
+ */
+public class Content {
+    
+    
+    public void post()
+    {
+        
+    }
+    public void story()
+    {
+        
+    }
+    
+}

87b1d3331d738a00d46862020f64e98c896ef64b
SeifShaheen <82758372+SeifShaheen@users.noreply.github.com>
2024-12-05 22:35:36 +0200

    testing commit

---
+++ Connect-Hub/src/main/java/Backend/PostsFactory.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+
+public class PostsFactory implements Creation {
+
+    @Override
+    public Contents createContent(String text) {
+        return new Post(text);
+    }
+
+    @Override
+    public Contents createContent(String text, String imagePath) {
+        return new Post(text,imagePath);
+    }
+    
+}
--- Connect-Hub/src/main/java/com/mycompany/connect/hub/ConnectHub.java
+++ Connect-Hub/src/main/java/com/mycompany/connect/hub/ConnectHub.java
@@ -1,23 +1,30 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
+
+import java.util.ArrayList;
+import Backend.Contents;
+import Backend.Creation;
+import Backend.Post;
+import Backend.PostsFactory;
+import Backend.Story;
+import Backend.StoryMonitor;
 package com.mycompany.connect.hub;
 
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 
 /**
  *
  * @author seifs
  */
-//Files problem
 public class ConnectHub {
 
     static User currentUser = null;
-
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException, InterruptedException {
-          new ProjectsOpening().setVisible(true);
-//        new NewsfeedPage().setVisible(true);
-
+        //new ProjectsOpening().setVisible(true);
+        StoryMonitor.checkExpiredStories();
+        User x= new CreateUser().information("reem@gmail.com", "reem ibrahim", "000", "2004-10-22");
+        System.out.println(x.toString());
+        x.createPost("yarab satrak");
+        x.createStory("ostor yarab");        
     }
 }

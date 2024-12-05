cf7905f1377fe54eed204d3d0d7904ea6ea600ec
Nada Zweta <154322441+nadazweta@users.noreply.github.com>
2024-12-05 21:41:28 +0200

    Done Profile Management

--- Connect-Hub/src/main/java/com/mycompany/connect/hub/ConnectHub.java
+++ Connect-Hub/src/main/java/com/mycompany/connect/hub/ConnectHub.java
@@ -11,6 +11,8 @@ package com.mycompany.connect.hub;
 public class ConnectHub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       // System.out.println("Hello World!");
+       ProfileManagmentPage profileManagmentPage=new ProfileManagmentPage();
+        
     }
 }
